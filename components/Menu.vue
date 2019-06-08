19d0f09094831b5c00c5215f94f76cea77880773
Khrongpop <mr.khrongpop@gmail.com>
2019-06-08 16:34:34 +0700

    animate button

--- components/Menu.vue
+++ components/Menu.vue
@@ -2,10 +2,7 @@
   <div id="menu">
     <div class="clearfix mn-50">
       <div class="hamburger">
-        <img
-          src="../assets/images/menu/ham-hover.png"
-          @click="menuTrigger()"
-        >
+        <img src="../assets/images/menu/ham-hover.png" @click="menuTrigger()">
       </div>
       <transition name="fade">
         <ul class="menu-list" v-show="!showHamburger">
@@ -14,6 +11,11 @@
           </li>
         </ul>
       </transition>
+
+      <aside class="loading">
+        <span class="text">Loading</span>
+        <span class="border"></span>
+      </aside>
     </div>
 
     <menu-trigger :status="showHamburger" v-if="click" :menus="links"/>
@@ -27,12 +29,12 @@ export default {
   data() {
     return {
       links: [
-        { name: "home", path:'index',icon:'001-home'},
-        { name: "creator",icon:'002-user'},
-        { name: "book", icon:'003-ebook'},
-        { name: "exhibition", icon:'004-tickets'},
-        { name: "thankyou",  icon:'005-handshake'},
-        { name: "contact", icon: '006-contact'}
+        { name: "home", path: "index", icon: "001-home" },
+        { name: "creator", icon: "002-user" },
+        { name: "book", icon: "003-ebook" },
+        { name: "exhibition", icon: "004-tickets" },
+        { name: "thankyou", icon: "005-handshake" },
+        { name: "contact", icon: "006-contact" }
       ],
       showHamburger: false,
       click: false
@@ -50,3 +52,99 @@ export default {
 };
 </script>
 
+<style lang="less" scoped>
+// @import url(https://fonts.googleapis.com/css?family=The+Girl+Next+Door);
+
+.wrapper {
+  padding: 30px;
+}
+aside {
+  margin: auto;
+  width: 200px;
+}
+
+@hueRotate: 10deg;
+
+.loading {
+  // font-family: "The Girl Next Door", cursive;
+  font-size: 26px;
+  padding: 3px;
+
+  background-size: 130%;
+  -webkit-filter: hue-rotate(@hueRotate);
+  height: 50px;
+  line-height: 46px;
+  color: white;
+  border-radius: 8px;
+  text-align: center;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.4) inset;
+
+  text-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+
+  position: relative;
+}
+
+.text,
+.border {
+  z-index: 5;
+  border-radius: 10px;
+  display: block;
+  position: absolute;
+  left: 4px;
+  top: 4px;
+  bottom: 4px;
+  right: 4px;
+  display: block;
+}
+
+.text {
+  top: 7px;
+}
+.border {
+  border: 2px dashed rgba(255, 255, 255, 1);
+  clip: rect(0, 0, 0, 0);
+
+  animation: maskBorder 1.4s infinite;
+}
+
+@keyframes maskBorder {
+  0% {
+    clip: rect(0, 50px, 10px, 0);
+  }
+  20% {
+    clip: rect(0, 200px, 10px, 0);
+  }
+  35% {
+    clip: rect(0, 200px, 10px, 190px);
+  }
+  50% {
+    clip: rect(10px, 200px, 50px, 190px);
+  }
+  60% {
+    clip: rect(40px, 200px, 50px, 190px);
+  }
+  70% {
+    clip: rect(40px, 200px, 50px, 0px);
+  }
+  85% {
+    clip: rect(40px, 10px, 50px, 0px);
+  }
+  90% {
+    clip: rect(0, 10px, 50px, 0px);
+  }
+  100% {
+    clip: rect(0, 10px, 10px, 0px);
+  }
+}
+
+body {
+  background: #bbddee;
+  background: radial-gradient(ellipse cover, #fff 0%, #bbddee 100%);
+  padding: 50px 100px;
+  height: 500px;
+}
+
+.loading {
+  background-color: #36c4e6;
+}
+</style>
