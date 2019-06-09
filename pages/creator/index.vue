56d1a4a9aa4167590ea9e45722550e8fc601e61a
Khrongpop <mr.khrongpop@gmail.com>
2019-06-09 23:06:49 +0700

    carousel

--- pages/creator/index.vue
+++ pages/creator/index.vue
@@ -1,12 +1,73 @@
 <template>
   <div>
-    <b-row>
+    <div>
+      <div style="width:30%" class="float-left">sdsds</div>
+      <div style="width:70%" class="float-right">
+        <carousel :per-page="5" :paginationEnabled="false" :loop="true" :autoplay="false">
+          <slide v-for="(creator, index) in creators" :key="index">
+            <!-- <creator-card :vdata="creator" class="mt-5"/> -->
+            <div v-if="index < 5">
+              <b-col v-if="index % 5 === 0">
+                <creator-card :vdata="creator" class="mt-5"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 1">
+                <creator-card :vdata="creator"/>
+                <creator-card :vdata="creator"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 2" class="mt-5">
+                <creator-card :vdata="creator"/>
+                <creator-card :vdata="creator"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 3">
+                <creator-card :vdata="creator"/>
+                <creator-card :vdata="creator"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 4">
+                <creator-card :vdata="creator" class="mt-5"/>
+              </b-col>
+            </div>
+
+            <div v-else-if="index < 10">
+              <b-col v-if="index % 5 === 0">
+                <creator-card :vdata="creator"/>
+                <creator-card :vdata="creator"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 1" class="mt-5">
+                <creator-card :vdata="creator"/>
+                <creator-card :vdata="creator"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 2">
+                <creator-card :vdata="creator"/>
+                <creator-card :vdata="creator"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 3">
+                <creator-card :vdata="creator" class="mt-5"/>
+              </b-col>
+
+              <b-col v-else-if="index % 5 === 4">
+                <creator-card :vdata="creator"/>
+                <creator-card :vdata="creator"/>
+              </b-col>
+            </div>
+          </slide>
+        </carousel>
+      </div>
+      <span class="clearfix"></span>
+    </div>
+    <!-- <b-row>
+      <br>
+      <br>
       <div style="width:30%" class="float-left">sdsds</div>
       <div style="width:70%" class="float-right">
         <b-row>
           <div v-for="(creator,index) in creators" :key="creator.id">
-            <!-- <creator-card :vdata="creator"/> -->
-            <!-- <b-col v-if="index === 0"> -->
             <div v-if="index < 5">
               <b-col v-if="index % 5 === 0">
                 <creator-card :vdata="creator" class="mt-5"/>
@@ -83,27 +144,27 @@
             </div>
           </div>
 
-          <!-- <b-col>
-        <creator-card :vdata="creators[0]" class="mt-5"/>
-      </b-col>
-      <b-col>
-        <creator-card :vdata="creators[0]"/>
-        <creator-card :vdata="creators[0]"/>
-      </b-col>
-      <b-col>
-        <creator-card :vdata="creators[0]" class="mt-5"/>
-        <creator-card :vdata="creators[0]"/>
-      </b-col>
-      <b-col>
-        <creator-card :vdata="creators[0]"/>
-        <creator-card :vdata="creators[0]"/>
-      </b-col>
-      <b-col>
-        <creator-card :vdata="creators[0]" class="mt-5"/>
-          </b-col>-->
+          <b-col>
+            <creator-card :vdata="creators[0]" class="mt-5"/>
+          </b-col>
+          <b-col>
+            <creator-card :vdata="creators[0]"/>
+            <creator-card :vdata="creators[0]"/>
+          </b-col>
+          <b-col>
+            <creator-card :vdata="creators[0]" class="mt-5"/>
+            <creator-card :vdata="creators[0]"/>
+          </b-col>
+          <b-col>
+            <creator-card :vdata="creators[0]"/>
+            <creator-card :vdata="creators[0]"/>
+          </b-col>
+          <b-col>
+            <creator-card :vdata="creators[0]" class="mt-5"/>
+          </b-col>
         </b-row>
       </div>
-    </b-row>
+    </b-row>-->
   </div>
 </template>
 
