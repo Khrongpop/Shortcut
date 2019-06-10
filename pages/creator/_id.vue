2d862a0976c701c4b28b85feb928e9ecb149fd80
Muang <mr.khrongpop@gmail.com>
2019-06-10 11:08:18 +0700

    detial layout

--- pages/creator/_id.vue
+++ pages/creator/_id.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <div style="width:30%" class="float-left">sdsds</div>
+    <div style="width:70%" class="float-right mb-5">
+      <b-row>
+        <b-col>
+          SHOWREEL
+          <br>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/lELqMu5HCY0"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </b-col>
+        <b-col>TECHNOLOGY</b-col>
+      </b-row>
+    </div>
     <pre class="text-white">
         {{creator(id)}}
     </pre>
@@ -7,17 +25,17 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 export default {
-  layout: 'topMenu',
-  computed : {
+  layout: "topMenu",
+  computed: {
     id() {
-      return parseInt(this.$route.params.id)
+      return parseInt(this.$route.params.id);
     },
     ...mapGetters({
-      creator: 'creator/getCreatorById'
+      creator: "creator/getCreatorById"
     })
   }
-}
+};
 </script>
 
