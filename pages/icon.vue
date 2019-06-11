6e454d90df6e6e341fabfb3ffe36220c90ce0a3d
Arm <mougnoi_k@silpakorn.edu>
2019-06-11 17:39:18 +0700

    sample

--- pages/icon.vue
+++ pages/icon.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    <dev-icon name="android" :full="false" size="2"/>
+    <dev-icon name="android" size="5"/>
     <dev-icon name="android" :full="true" size="5"/>
     <dev-icon name="swift" :full="true" size="5"/>
-    <dev-icon name="swift" :full="false" size="5"/>
-    <dev-icon name="vuejs" :full="false" size="5"/>
+    <dev-icon name="swift"  size="5"/>
+    <dev-icon name="vuejs" size="5"/>
+    <dev-icon name="laravel" :full="true" size="5"/>
+    <dev-icon name="php" size="5"/>
+    <dev-icon name="go" size="5"/>
+    <dev-icon name="react" size="5"/>
+    <dev-icon name="java" size="5"/>
+    <dev-icon name="csharp" size="5"/>
+    <dev-icon name="express" size="5"/>
+    <dev-icon name="mongodb" size="5"/>
+
 
   </div>
 </template>
