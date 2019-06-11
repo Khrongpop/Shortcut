97ba53ed8bf23945d23d8a8473e038ab2ed935c3
Arm <mougnoi_k@silpakorn.edu>
2019-06-11 17:31:13 +0700

    make dev icon component

---
+++ components/DevIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-white">
+    <i :class="`devicon-${name}-plain${full?'-wordmark':''}`" :style="`font-size:${size}em;`"></i>
+  </div>
+</template>
+
+<script>
+export default {
+props:['name','full','size']
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <dev-icon name="android" :full="false" size="2"/>
+    <dev-icon name="android" :full="true" size="5"/>
+    <dev-icon name="swift" :full="true" size="5"/>
+    <dev-icon name="swift" :full="false" size="5"/>
+    <dev-icon name="vuejs" :full="false" size="5"/>
+
+  </div>
+</template>
+
+<script>
+import DevIcon from '../components/DevIcon'
+export default {
+  components:{
+    DevIcon
+  }
+}
+</script>
+
+<style>
+
+</style>
