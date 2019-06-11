8987fddc8f5f58e41bfbe6ae8b7dec6015486a34
Krit <moungnoi_k@silpakorn.edu>
2019-06-12 00:56:50 +0700

    contact

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,13 +1,83 @@
 <template>
-<div>
-  this is contact
+<div id="thankyou">
+    <div class="row">
+        <div class="col-md-2">
+       <img src="/shortcut_left.png" class="img-fluid">
+        </div>
+
+    </div>
 </div>
 </template>
 
 <script>
+import Logo from '~/components/Logo'
 export default {
   layout: 'topMenu',
-
+  data() {
+    return {
+      links1: [
+        {
+        text:'หลักสูตรเทคโนโลยีสารสนเทศธุรกิจ',
+        link:'#'
+        },
+         {
+        text:'หลักสูตรเทคโนโลยีสารสนเทศเพื่อการออกแบบ',
+        link:'#'
+        },
+         {
+        text:'หลักสูตรเทคโนโลยีสารสนเทศนิเทศศาสตร์',
+        link:'#'
+        },
+        {
+        text:'ติดต่อคณะ',
+        link:'#'
+        },
+        {
+        text:'ไอซีทีศิลปากรโชว์เคส(ICT Showcase)',
+        link:'#'
+        },
+        ],
+        links2: [
+        {
+        text:'ICTSUWEB#05',
+        link:'#'
+        },
+         {
+        text:'ICTSUWEB#06',
+        link:'#'
+        },
+         {
+        text:'ICTSUWEB#07',
+        link:'#'
+        },
+        {
+        text:'ICTSUWEB#08',
+        link:'#'
+        },
+        {
+        text:'ICTSUWEB#09 - SURAROUND 9.1',
+        link:'#'
+        },
+         {
+        text:'ICTSUWEB#10 - OATHLON 10.2',
+        link:'#'
+        },
+        {
+        text:'ICTSUWEB#11 - Ultra-Line 11.3',
+        link:'#'
+        },
+        ],
+        links3:[
+          {name:'facebook.com/ictsilpakorn/',link:'facebook.com/ictsilpakorn/'},
+          {name:'twitter.com/ictsilpakorn/',link:'twitter.com/ictsilpakorn/'},
+          {name:'instagram.com/ictsilpakorn/',link:'instagram.com/ictsilpakorn/'},
+          {name:'youtube.com/ictsilpakorn/',link:'youtube.com/ictsilpakorn/'},
+          {name:'vimeo.com/ictsilpakorn',link:'vimeo.com/ictsilpakorn'},
+          {name:'ict.su.ac.th',link:'vimeo.com/ictsilpakorn'},
+        ],
+        links4:{name:'facebook.com/func/',link:'Shotcut 13.5'}
+    }
+  }
 }
 </script>
 
