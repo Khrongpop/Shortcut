ef06d51ccefea90bf89f4dde18327635a65a3007
Khrongpop <mr.khrongpop@gmail.com>
2019-06-13 02:47:34 +0700

    exhibi layout

--- pages/exhibition/index.vue
+++ pages/exhibition/index.vue
@@ -3,12 +3,47 @@
     <div>
       <div style="width:5%;" class="float-left">อ่านทำไม</div>
       <carousel :per-page="1" :paginationEnabled="false" :loop="true" :autoplay="false">
-        <slide>
-          <div style="width:70%" class="float-left"></div>
+        <slide class="exhibition-section-1">
+          <Logo/>
+          <div class="video">sdsd</div>
+
+          <div class="bottom-content-1">
+            <img :src="`${baseURL}/creators/detail/bulet.png`" alt>
+
+            <div class="img-key"></div>
+            <div class="bottom-left-content-1">
+              <div class="img-title"></div>
+              <p>
+                คณะเทคโนโลยีสารสนเทศและการสื่อสาร มหาวิทยาลัยศิลปากåร มหาวิทยาลัยชั้น
+                นำแห่งการสร้างสรรค์ ได้เล็งเห็นความสำคัญของการนำระบบเทคโนโลยีสารสนเทศ
+                และการสื่อสารเข้ามาใช้ให้เกิดประโยชน์ มุ่งหวังการผลิตบัณฑิตในสาขาวิชาที่สามารถ
+                ประยุกต์ความรู้และทักษะการบูรณาการศิลปะการออกแบบ และเทคโนโลยีเข้าด้วยกัน
+                เพื่อตอบโจทย์ความต้องการของประเทศที่กำลังก้าวเข้าสู่ยุคเศรษฐกิจดิจิทัลอย่างเต็มตัว
+              </p>
+            </div>
+          </div>
         </slide>
-        <slide>
-          <div style="width:50%" class="float-left item-2">
-            <b-row></b-row>
+        <slide class="exhibition-section-2">
+          <div class="bottom-content-1">
+            <div class="slide"></div>
+
+            <div class="bottom-left-content-2">
+              <div class="img-title">
+                <img :src="`${baseURL}/creators/detail/bulet.png`" alt>
+                <div class="img"></div>
+              </div>
+              <p>
+                ทางลัด คือตัวช่วย ทางออก วิธี ดีที่สุด ทางเลือก เปรียบเทียบกับวิธีอื่นที่ดีน้อยกว่า
+                อัจฉริยะ คือมีความรู้ แก้ไขปัญหา เป็นที่น่าแปลกใจ ไม่จำเป็นต้องซับซ้อน ไม่จำเป็นต้องยาก เป้าหมาย คือ
+                ปัญหาในชีวิตประจำวันหรือเรื่องราวเล็กๆน้อยๆ ไม่จำเป็นต้องเป็นเรื่องใหญ่
+              </p>
+              <p>
+                ให้ก้าวล้ำเส้นแถวหน้าในกระแสวิวัฒน์ของสังคมโลก มีงานผ่านผลงานทั้ง 35 ชิ้น
+                โดยแต่ละชิ้นล้วนมีความแตกต่างและโดดเด่น สะท้อนองค์ความรู้ด้านการออกแบบและพัฒนางานให้ออกมาใช้ได้จริง
+                บนสื่อดิจิทัลที่ทันสมัย ตอบโจทย์วงการวิชาการและวิชาชีพ
+              </p>
+              <span>*หมายเลข 13.5 มีนัยยะแฝงสองส่วนคือ สื่อถึงรุ่นที่ 13</span>
+            </div>
           </div>
         </slide>
       </carousel>
@@ -18,8 +53,12 @@
 </template>
 
 <script>
+import Logo from "~/components/Logo";
 export default {
-  layout: "topMenu"
+  layout: "topMenu",
+  components: {
+    Logo
+  }
 };
 </script>
 
