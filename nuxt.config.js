4e38423b4ac9dd5f07e70e3d9a957ba34909b76b
Arm <mougnoi_k@silpakorn.edu>
2019-07-01 12:06:47 +0700

    248

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,8 +58,8 @@ module.exports = {
   ],
 
   router: {
-    routeNameSplitter: '/func/shortcut/',
-    base: '/func/shortcut/',
+    // routeNameSplitter: '/func/shortcut/',
+    // base: '/func/shortcut/',
     extendRoutes (routes, resolve) {
       routes.push({
         name: 'custom',
@@ -79,9 +79,9 @@ module.exports = {
   //  publicPath: 'https://www.ict.su.ac.th/func/shortcut/',
   // srcDir:'https://www.ict.su.ac.th/func/shortcut/',
   // rootDir:'https://www.ict.su.ac.th/func/shortcut/',
-   publicPath: '/func/shortcut/',
-   srcDir:'/func/shortcut/',
-   rootDir:'/func/shortcut/',
+  //  publicPath: '/func/shortcut/',
+  //  srcDir:'/func/shortcut/',
+  //  rootDir:'/func/shortcut/',
     extend(config, ctx) {
     }
   }
